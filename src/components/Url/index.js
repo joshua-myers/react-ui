12e5aa39216771c3721d0e0a6cfd81c91b222cbb
joshua-myers <100009956+joshua-myers@users.noreply.github.com>
2022-02-25 22:05:57 -0600

    Create basic form.

--- src/components/Url/index.js
+++ src/components/Url/index.js
@@ -1,10 +1,34 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import { Formik, Form, Field } from "formik";
 import { UrlList } from "./UrlList";
-
 export const Url = () => {
   return (
     <Grid fluid>
-      <Grid item></Grid>
+      <Grid item>
+        <Formik
+          initialValues={{ url: "", slug: "" }}
+          validate={async ({ url, slug }) => {
+            const errors = {};
+            if (!url) {
+              errors.url = "A URL is required.";
+            }
+            if (slug) {
+              // validate slug
+            }
+            return errors;
+          }}
+          onSubmit={({ url, slug }) => {
+            return true;
+          }}
+        >
+          <Form>
+            <Field name="url" placeholder="Make your links shorter" />
+            <Button variant="contained" type="submit">
+              Shorten URL
+            </Button>
+          </Form>
+        </Formik>
+      </Grid>
       <Grid item>
         <UrlList />
       </Grid>
