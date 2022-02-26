293c5431f832dc56cd540dcdabb88c10cdc5958e
joshua-myers <100009956+joshua-myers@users.noreply.github.com>
2022-02-26 01:35:18 -0600

    Fill out top header bar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,71 @@
-import { Grid, Link } from "@mui/material";
+import { AppBar, Button, Grid, Link, Toolbar, IconButton } from "@mui/material";
 import logo from "../../assets/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const Header = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item>
-        <img src={logo} alt="logo" />
-      </Grid>
-      <Grid item>
-        <Link href="/Features">Features</Link>
-        <Link href="/Domains">Domains</Link>
-        <Link href="/Pricing">Pricing</Link>
-      </Grid>
-    </Grid>
+    <AppBar position="sticky" color="inherit" sx={{ top: 0 }}>
+      <Toolbar>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <img src={logo} alt="logo" />
+          </Grid>
+          <Grid item sx={{ display: { md: "block", sm: "none", xs: "none" } }}>
+            <Grid container spacing={4}>
+              <Grid item>
+                <Link color="inherit" underline="hover" href="/features">
+                  Features
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link color="inherit" underline="hover" href="/domains">
+                  Domains
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link color="inherit" underline="hover" href="/Pricing">
+                  Pricing
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link color="inherit" underline="hover" href="/enterprise">
+                  Enterprise
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ display: { md: "block", sm: "none", xs: "none" } }}>
+            <Grid container spacing={4}>
+              <Grid item>
+                <Link color="inherit" underline="hover" href="/login">
+                  Login
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link color="inherit" underline="hover" href="/signup">
+                  Sign Up
+                </Link>
+              </Grid>
+              <Grid item>
+                <Button color="inherit" variant="outlined">
+                  Get a Quote
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ display: { md: "none", sm: "block", xs: "block" } }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
